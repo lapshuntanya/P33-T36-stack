71d54482ed44e85226cd72cd755509f52e99a7c8
lapshuntanya <lapshun.tatiana@gmail.com>
2024-11-27 17:13:30 +0200

    main

--- P33-T36-stack/P33-T36-stack.cpp
+++ P33-T36-stack/P33-T36-stack.cpp
@@ -1,7 +1,27 @@
-#include <iostream>
+#include "MyStack.h"
+#include <string>
+using std::string;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    using namespace myStack;
+
+    Stack<string> p33;//LIFO
+    p33.push_back("Students");
+    p33.push_back("of");
+    p33.push_back("the");
+    p33.push_back("group");
+    p33.push_back("P33");
+    p33.showStack();
+    cout << "------------------------\n\n";
+    p33.pop_back();
+    p33.showStack();//
+    cout << "------------------------\n\n";
+    //Розвантаження стеку (обробка стеку)
+    while (!p33.isEmpty()) {
+        cout << "# " << p33.top() << endl;
+        p33.pop_back();
+    }
+    p33.showStack();
 }
 
